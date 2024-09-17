820ece97c5d558da5c255e7f281242f1abb24849
yoonju88 <yoonju.t@gmail.com>
2024-09-17 10:58:40 +0200

    handle error for meals page

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+'use client' // important to mention use client
+
+export default function Error({error}) {
+    return (
+        <>
+            <main className="error">
+                <h1>An error occurred!</h1>
+                <p>Failed to fetch meal data, please try again later.</p>
+            </main>
+        </>
+    )
+}
