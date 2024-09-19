1e4efb3de30053afdd6dfdd9e4d9a3bcb32ff57f
yoonju88 <yoonju.t@gmail.com>
2024-09-19 13:41:47 +0200

    add meta data

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,17 @@ import  {getMeal} from '../../../lib/meals'
 import classes from "./page.module.css"
 import Image from 'next/image'
 
+export async function generateMetadata({params}) {
+    const meal =getMeal(params.mealSlug)
+    if (!meal){
+        notFound()
+    }
+    return {
+        title: meal.title,
+        description : meal.summary,
+    }
+}
+
 export default function MealDetailsPage({params}){
     const meal = getMeal(params.mealSlug)
     if (!meal){
