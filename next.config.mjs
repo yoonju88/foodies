13624743fa56566bf76379a39b121ae6bb9fe13e
yoonju88 <yoonju.t@gmail.com>
2024-10-15 10:30:25 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,   // React의 Strict Mode 활성화 (에러 검출에 유용)
+    swcMinify: true,         // SWC를 사용한 코드 최소화 (빌드 최적화)
+    poweredByHeader: false,  // X-Powered-By 헤더 비활성화 (보안 강화)
+    async redirects() {
+      return [
+        {
+          source: '/old-page',  // 잘못된 페이지 경로가 있는 경우 리디렉션 설정
+          destination: '/new-page',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
